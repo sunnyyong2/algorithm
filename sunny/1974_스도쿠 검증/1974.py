61cb3c768656920fd86d2c4dcda930709ed53557
sunny <sunnyyong2@gmail.com>
2019-09-05 17:59:41 +0900

    20190905

---
+++ sunny/1974_스도쿠 검증/1974.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+for tc in range(1,T+1):
+    N = [list(map(int,input().split()))]
+
+    I = set()
+    for i in range(9):
+        # number = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        
+print(len(I))
+
+        # for j in range(9):
+
